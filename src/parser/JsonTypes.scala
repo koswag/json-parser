2ceee8e93014bb01e71d5e9e900940a718a2c3d2
Konstanty Skarżyński <k.skarzynski@techocean.pl>
2020-08-04 23:45:14 +0200

    Enhanced JSON types

--- src/parser/JsonTypes.scala
+++ src/parser/JsonTypes.scala
@@ -2,24 +2,56 @@ package parser
 
 object JsonTypes {
 
-    sealed trait JsonValue
+    sealed trait JsonValue {
+        type A
 
-    case object JsonNull
-        extends JsonValue
+        def value: A
 
-    case class JsonBool(value: Boolean)
-        extends JsonValue
+        def as[T <: JsonValue]: T =
+            this.asInstanceOf[T]
+    }
 
-    case class JsonNumber(value: Int)
-        extends JsonValue
+    case object JsonNull extends JsonValue {
+        override type A = Null
 
-    case class JsonString(value: String)
-        extends JsonValue
+        override def value: A = null
+    }
 
-    case class JsonArray(value: List[JsonValue])
-        extends JsonValue
+    case class JsonBool(value: Boolean) extends JsonValue {
+        override type A = Boolean
+    }
 
-    case class JsonObject(value: List[(List[Char], JsonValue)])
-        extends JsonValue
+    case class JsonNumber(value: Int) extends JsonValue { // no Double
+        override type A = Int
+    }
+
+    case class JsonString(value: String) extends JsonValue {
+        override type A = String
+    }
+
+    case class JsonArray(value: List[JsonValue]) extends JsonValue {
+        override type A = List[JsonValue]
+
+        def getValues: List[Any] =
+            valuesOf(this).asInstanceOf[List[Any]]
+    }
+
+    case class JsonObject(value: List[(List[Char], JsonValue)]) extends JsonValue {
+        override type A = List[(List[Char], JsonValue)]
+
+        def getValues: Map[String, Any] =
+            valuesOf(this).asInstanceOf[Map[String, Any]]
+    }
+
+    def valuesOf(json: JsonValue): Any = json match {
+        case obj: JsonObject =>
+            obj.value.map {
+                case (k, v) =>
+                    k.mkString -> valuesOf(v)
+            }
+        case arr: JsonArray =>
+            arr.value map valuesOf
+        case _ => json.value
+    }
 
 }
