26cb438366fa686ab8c5e398227945e22ffccf5b
Konstanty Skarżyński <k.skarzynski@techocean.pl>
2020-08-05 23:26:18 +0200

    Created test application

--- src/main/Main.scala
+++ src/main/Main.scala
@@ -1,5 +1,33 @@
 package main
 
+import parser.JsonParser
+import parser.JsonTypes.JsonObject
+
+import scala.io.Source
+import scala.util.{Try, Using}
+
 object Main extends App {
+
+    def readFile(path: String): Try[String] =
+        Using(Source.fromFile(path)) {
+            file =>
+                file.getLines().mkString
+        }
+
+
+    for (contents <- readFile("resources/test.json")) {
+
+        val json: Option[JsonObject] =
+            for {
+                (_, json) <- JsonParser(contents)
+            } yield json.as[JsonObject]
+
+        println(json)
+
+//        for (contents <- json) {
+//            contents.getValues foreach println
+//        }
+
+    }
 
 }
